8f0f309dca1d07c8115a20dfc3787eed1d5336d3
MaidDragon <1003269315@qq.com>
2020-02-18 19:59:49 +0800

    (2.17)还在改bug

---
+++ JAVA学习/(2.17)还在改bug/src/main/java/com/example/demo/domain/ListInput.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+/*这个用来传参*/
+public class ListInput {
+    private List<Integer> stuid;
+
+    public List<Integer> getStuid() {
+        return stuid;
+    }
+
+    public void setStuid(List<Integer> stuid) {
+        this.stuid = stuid;
+    }
+}
---
+++ JAVA学习/(2.17)还在改bug/src/main/java/com/example/demo/mapper/StudentMapper.java
@@ -0,0 +1,34 @@
+package com.example.demo.mapper;
+
+
+import com.example.demo.domain.ListInput;
+import com.example.demo.domain.StudentDomain;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper  //映射不能忘了Mapper
+public interface StudentMapper {
+
+    List<StudentDomain> findStudntByName(String stuname);
+
+    List<StudentDomain> ListStudnt();
+
+    int insertStudent(StudentDomain studentDomain);
+
+    int delete(int stuid);
+   /*批量插入*/
+   int batchInsert(List<StudentDomain> list);//批量插入
+  /*  批量更新*/
+  int batchUpdate(List<StudentDomain> list);//批量更新
+/*动态aql之<if>*/
+List<StudentDomain> findByUser(StudentDomain studentDomain);
+/*查询stuid集合*/
+    List<StudentDomain> findInIds(ListInput listInput);
+    /*查询stuid集合*/
+    List<StudentDomain> findAllAccount(int stuid);
+/*一对一*/
+List<StudentDomain> findAllAccountByid(int stuid);
+}
+
+
