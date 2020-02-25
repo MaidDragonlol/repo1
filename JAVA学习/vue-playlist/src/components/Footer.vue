39d30baac61ef7b6e0826f2c2432ba087c114d89
MaidDragon <1003269315@qq.com>
2020-02-25 19:31:41 +0800

    (2.25)vue练习项目

---
+++ JAVA学习/vue-playlist/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer calss="app-footer">
+    <p>{{footer}}</p>
+  </footer>
+</template>
+<script>
+  export default {
+    name: "app-footer",
+    data(){
+      return {
+        footer:"welcom! "
+      }
+    }
+  }
+</script>
+<style scoped>
+  footer{
+    background-color: lightblue;
+    padding:14px;
+  }
+  p{
+    color: grey;
+    test-align: center;
+  }
+</style>
