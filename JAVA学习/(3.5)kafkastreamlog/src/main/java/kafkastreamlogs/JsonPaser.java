6e228aaaefefd74eebf3c3b9a347a31a35583512
MaidDragon <1003269315@qq.com>
2020-03-05 19:35:44 +0800

    (3.5)kafkastreamlog

--- JAVA学习/(3.5)kafkastreamlog/src/main/java/kafkastreamlogs/JsonPaser.java
+++ JAVA学习/(3.5)kafkastreamlog/src/main/java/kafkastreamlogs/JsonPaser.java
@@ -14,8 +14,6 @@ import java.util.concurrent.TimeUnit;
 public class JsonPaser {
     static LogCutQuery logCutQuery = new LogCutQuery();
     static LogCutRecommend logCutRecommend = new LogCutRecommend();
-    static int ire = 1;
-
     /*对节点进行解析*/
     private static JSONObject getObj(JSONObject obj, String node) {
         try {
@@ -78,9 +76,12 @@ public class JsonPaser {
         return jsonPaths;
     }
 
-    public static void InsertToBean(String mothod, String everyContent) {
-        if (mothod == "TuniuController.query(..)") {
-            switch (everyContent) {
+    public static void InsertToBean(String mothod, String[] everyMap) {
+        String node = everyMap[0];
+        String everyContent = everyMap[1];
+
+        if (mothod.equals("TuniuController.query(..)")) {
+            switch (node) {
                 case "time": {
                     logCutQuery.setTime(everyContent);
                 }
@@ -110,8 +111,9 @@ public class JsonPaser {
                 }
                 break;
             }
-        } else if (mothod == "JintongController.recommend(..)") {
-            switch (everyContent) {
+        } else if (mothod.equals("JintongController.recommend(..)")) {
+
+            switch (node) {
                 case "time": {
                     logCutRecommend.setTime(everyContent);
                 }
@@ -145,7 +147,7 @@ public class JsonPaser {
 
     }
 /*这个进行递归运算*/
-    public static String recursive(JSONObject obj, String jsonPath, int i) throws JSONException {
+    public static String[] recursive(JSONObject obj, String jsonPath, int i) throws JSONException {
         String[] nodes = jsonPath.split("\\.");
         JSONObject objElemet = getObj(obj, nodes[i]);
         if (i < nodes.length - 1) {
@@ -154,9 +156,9 @@ public class JsonPaser {
         }
         if ((i + 1) == nodes.length) {
             String everyContent = objElemet.getString(nodes[i]);
-            System.out.println(everyContent);
-            /*元素提取后判断根据method如何哪个标准类*/
-            return everyContent;
+            /*这里把键值对存入map后面存入标准类的时候判断要用*/
+            String[] everyMap = new String[]{nodes[i],everyContent};
+            return everyMap;
         }
 
         return null;
@@ -179,8 +181,8 @@ public class JsonPaser {
         geyNodeInfo(jsonPaths, mothod);
         /*遍历jsonPaths读取一一对应属性一一存入标准类*/
         for (String jsonPath : jsonPaths) {
-            String everyContent = recursive(obj, jsonPath, 1);
-            InsertToBean(mothod, everyContent);
+            String[] everyMap = recursive(obj, jsonPath, 1);
+            InsertToBean(mothod, everyMap);
         }
         return null;
     }
@@ -193,9 +195,12 @@ public class JsonPaser {
         String jsonContent2 = "{\"time\":\"2020-02-28 10:18:58\", \"thread\":\"http-nio-8000-exec-24\",\"class\":\"com.yyc.gateway.configuration.aspect.ControllerLogAspect\",\"categoryId\":\"gateway-service\",\"level\":\"WARN\",\"message\":{\"method\":\"TuniuController.query(..)\",\"startTime\":\"2020-02-28T10:18:56.753\",\"startMills\":1582856336753,\"elapseMills\":2243,\"Args\":[{\"cid\":\"cxtuniu\",\"tripType\":1,\"fromCity\":\"DFW\",\"toCity\":\"CHI\",\"fromDate\":\"20200228\",\"retDate\":\"\",\"cabinClass\":1,\"adultNumber\":1,\"childNumber\":0}],\"result\":{\"status\":999,\"msg\":\"search_lack_of_data_error\",\"routings\":null},\"error\":false,\"exMsg\":null,\"ota\":\"TUNIU\",\"stage\":\"QUERY\",\"path\":\"/tuniu/flight/v1/query\",\"remoteAddr\":\"180.97.2.17\"}}";
         String jsonContent = "{\"time\":\"2020-02-28 10:23:51\", \"thread\":\"http-nio-8000-exec-18\",\"class\":\"com.yyc.gateway.configuration.aspect.ControllerLogAspect\",\"categoryId\":\"gateway-service\",\"level\":\"WARN\",\"message\":{\"method\":\"JintongController.recommend(..)\",\"startTime\":\"2020-02-28T10:23:48.839\",\"startMills\":1582856628839,\"elapseMills\":2397,\"args\":[{\"cid\":\"cxjinri\",\"tripType\":1,\"adultNum\":1,\"childrenNum\":0,\"routing\":{\"adultPrice\":0,\"adultTax\":0,\"adultTaxType\":0,\"applyType\":0,\"priceType\":0,\"autoTicket\":0,\"childPrice\":0,\"childTax\":0,\"ticketTimeLimit\":0,\"childTaxType\":0,\"infantPrice\":0,\"infantTax\":0,\"data\":\"123456\",\"fareType\":null,\"lastTicketDate\":null,\"nationality\":null,\"needChangePNR\":null,\"speFlag\":0,\"reservationType\":null,\"ticketType\":null,\"validatingCarrier\":null,\"ticketInvoiceType\":0,\"ticketInfo\":null,\"rule\":null,\"ruleEx\":null,\"fromSegments\":[{\"aircraftCode\":\"\",\"arrAirport\":\"PVG\",\"arrTime\":\"202003030945\",\"arrivingTerminal\":null,\"baggageAllowance\":0,\"baggagePieces\":0,\"cabin\":\"L\",\"cabinClass\":1,\"carrier\":\"SU\",\"codeShare\":false,\"depAirport\":\"SVO\",\"depTime\":\"202003021950\",\"duration\":0,\"flightNumber\":\"SU208\",\"seatCount\":0,\"stopCities\":\"\",\"sharingFlightNumber\":null,\"departureTerminal\":null}],\"retSegments\":[],\"fromLuggages\":null,\"retLuggages\":null},\"passengers\":[{\"name\":\"BILA/NATALIIA\",\"ageType\":0,\"birthday\":\"19890808\",\"gender\":\"F\",\"cardType\":\"PP\",\"cardNum\":\"E12369898\",\"cardExpired\":\"20280320\",\"cardIssuePlace\":\"UA\",\"nationality\":\"UA\"}]}],\"result\":{\"status\":0,\"msg\":\"无运价\",\"sessionId\":null,\"routing\":null,\"maxSeats\":0,\"rule\":null,\"routings\":null},\"error\":false,\"exMsg\":null,\"ota\":\"JINTONG\",\"stage\":\"RECOMMEND\",\"path\":\"/jinri/flight/v1/recommend\",\"remoteAddr\":\"61.151.247.215\"}}";
         JsonPaser.insertConent(jsonContent);
+
         long end = System.nanoTime();//获取系统结束的时间点
         long ms = TimeUnit.NANOSECONDS.toMillis(end - start);//得到所用的时间
-        System.out.println(ms + "ms");  //31ms
+        System.out.println(logCutRecommend);
+        System.out.println(logCutQuery);
+        System.out.println(ms + "ms");  //7ms
     }
 
 }
